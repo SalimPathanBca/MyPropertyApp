153e7d77ae9cf49b1d770a7bad0d57cd71a7903c
salim <salim.pathan.bca@gmail.com>
2023-02-06 00:57:05 +0000

    Initial Project

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PropertyCardComponent } from './property/property-card/Property-Card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
